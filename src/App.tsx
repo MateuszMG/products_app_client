c464f0d142a5f2909bb2ae6b46fd611f2259198d
Mateusz MG <MateuszMG.default@com.com>
2024-01-24 08:04:00 +0100

    feat(PAF-2): add changelog and husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { Profiler } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
-import { onRenderProfiler } from './helpers/profiler';
+
 import { ErrorBoundary } from './hocs/ErrorBoundary/ErrorBoundary';
-import { Routes } from './routes/Routes';
+
+import { onRenderProfiler } from './helpers/profiler';
+
 import { GlobalStyle } from './utils/theme/GlobalStyle';
 import { Theme, themeVariants } from './utils/theme/themeVariants';
+
+import { Routes } from './routes/Routes';
 
 export const App = () => {
   return (
